27e9b696da5335cd897529b9c4662910ea332cf0
Marcela Cruz <marcelacaroleine@hotmail.com>
2022-09-28 19:11:28 -0300

    atualizacaoServico

--- src/components/Pesquisa.jsx
+++ src/components/Pesquisa.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
 import { Form, FormControl, Button} from "react-bootstrap"
-export default function Pesquisa (){
+
+export default function Pesquisa () {
+  const [ busca , setBusca ] = useState('');
+
+
     return(
         <Form className="d-flex">
               <FormControl
                 type="Search"
                 placeholder="Procure um prestador próximo de você!"
                 aria-label="Search"
+                value={busca}
+                onChange={(env) => setBusca(env.target.value)}
               />
               <Button variant="secondary m-2 p-2">Pesquisar</Button>
             </Form>
