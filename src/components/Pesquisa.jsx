420a7a8e63b386948b0cab1ec4a13cd8378905a2
Marcela Cruz <marcelacaroleine@hotmail.com>
2022-09-29 19:19:37 -0300

    pesquisa

--- src/components/Pesquisa.jsx
+++ src/components/Pesquisa.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { Form, FormControl, Button} from "react-bootstrap"
+
+const servico = [
+  'faxina',
+  'pintura',
+  'manutenção',
+  'construção',
+  'reforma'
+];
 
 export default function Pesquisa () {
   const [ busca , setBusca ] = useState('');
-
-
+  const servicoFiltrados = useMemo (() => {
+    const lowerBusca = busca.toLowerCase();
+    return servico.filter((servico) => 
+    servico.toLowerCase().includes(lowerBusca)
+  );
+  }, [busca]);
+  
     return(
         <Form className="d-flex">
               <FormControl
-                type="Search"
+                type="text"
                 placeholder="Procure um prestador próximo de você!"
                 aria-label="Search"
                 value={busca}
---
+++ src/components/toasts/SaibaMais.jsx
@@ -0,0 +1,19 @@
+export default function SaibaMais () {
+    return("x")
+    // servico
+    // anuncio: anuncio,
+    // nomefantasia: nomefantasia,
+    // cnpj: cnpj,
+    // descricao: descricao,
+    // disponibilidade: disponibilidade,
+    // avaliacao: avaliacao,
+    // tipo: tipo,
+    // valor: valor,
+    // cidade: cidade,
+    // email: email,
+    // nomeproprietario: nomeproprietario,
+    // telefone: telefone
+    // feedback
+    // "deixe seu comentário"
+    
+}
