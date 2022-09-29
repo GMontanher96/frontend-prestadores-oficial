1dd8417c460d5488265fdeda991c7a669999142a
Baiduligado <baiduligado@gmail.com>
2022-09-29 19:07:54 -0300

    alteracoes home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import GlobalStyle from "./styles/global";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 import Footer from "./components/template/Footer/Footer"
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "./components/carrosel/style.css";
 
 
 const App = () => {
